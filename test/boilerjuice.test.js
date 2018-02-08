1bf6c4fba40f190768838517064fc8130e5b897d
roylines <roy@roylines.co.uk>
2018-02-08 19:40:16 +0000

    mocha -> jest

--- test/boilerjuice.test.js
+++ test/boilerjuice.test.js
@@ -1,6 +1,14 @@
 const expect = require('chai').expect
 const boilerjuice = require('../index.js');
 
+test('should return expected data', done => {
+  boilerjuice.get(7).then(ret => {
+    expect(ret).to.have.length(7);
+    return done();
+  });
+});
+
+/*
 describe('boilerjuice', () => {
   it('should not error', done => {
     boilerjuice.get(7).then(ret => {
@@ -9,3 +17,4 @@ describe('boilerjuice', () => {
     });
   });
 });
+*/
