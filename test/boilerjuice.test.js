0971cd6657c8da4bc709c5e4e4967f8ccc69cbb2
roylines <roy@roylines.co.uk>
2018-02-08 19:43:06 +0000

    corrected jest test

--- test/boilerjuice.test.js
+++ test/boilerjuice.test.js
@@ -1,20 +1,9 @@
-const expect = require('chai').expect
 const boilerjuice = require('../index.js');
 
 test('should return expected data', done => {
   boilerjuice.get(7).then(ret => {
-    expect(ret).to.have.length(7);
+    expect(ret).toHaveLength(7);
     return done();
   });
 });
 
-/*
-describe('boilerjuice', () => {
-  it('should not error', done => {
-    boilerjuice.get(7).then(ret => {
-      expect(ret).to.have.length(7);
-      return done();
-    });
-  });
-});
-*/
